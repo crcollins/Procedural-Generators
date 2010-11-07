d7cbb0cf2dca98e0f89df47447fe7d9899b274af
chris0076 <socom_man0076@yahoo.com>
2010-11-07 13:26:00 -0800

    Made an ~10% speed increase between Draw.py and MakeImage.py

--- hyperserv/Draw.py
+++ hyperserv/Draw.py
@@ -8,75 +8,46 @@ class Draw:
         self.size = size
         self.intPolType = intPolType
         
-    def getLocation(self, idNumber):
-        xloc = idNumber % (self.size)
-        yloc = idNumber / (self.size)
-        return (xloc, yloc)
-        
     def draw(self):
         self.img = Image.new("RGB", (self.size, self.size))
         draw = ImageDraw.ImageDraw(self.img)
-        self.minNumber = min(self.values)
-        self.maxNumber = max(self.values) - self.minNumber
-        for pixel in xrange((self.size) * (self.size)):
-            coords = self.getLocation(pixel)
-            self.values[pixel] = self.values[pixel] / self.maxNumber
-            number = self.interpolate(self.values[pixel])
-            color = int(255 * number)
-            colorSet = (color, color, color)
-            draw.point(coords, colorSet)
+        minnumbers = []
+        maxnumbers = []
+        for number in xrange(len(self.values)):
+            minnumbers.append(min(self.values[number]))
+            maxnumbers.append(max(self.values[number]))
+        self.maxNumber = max(maxnumbers) - min(minnumbers)
+        
+        for y in xrange(self.size):
+            for x in xrange(self.size):
+                coords = (x,y)
+                self.values[y][x] = self.values[y][x] / self.maxNumber
+                number = self.interpolate(self.values[y][x])
+                color = int(255 * number)
+                colorSet = (color, color, color)
+                draw.point(coords, colorSet)
         self.img.show()
 
     def write(self):
         for y in xrange(self.size):
             for x in xrange(self.size):
-                ID = self.getID((x,y))
-                string = "%.3f " % self.heights[ID]
+                string = "%.3f " % self.values[y][x]
                 sys.stdout.write(string)
             sys.stdout.write("\n")
 
     def interpolate(self, x):
         if self.intPolType == "sin":
-            ans = self.sin(x)
+            ans = math.sin(x)
         elif self.intPolType == "smoothStep":
-            ans = self.smoothStep(x)
+            ans = (3 * x)**2 - (2 * x)**3
         elif self.intPolType == "squared":
-            ans = self.squared(x)
+            ans = x**2
         elif self.intPolType == "sqrt":
-            ans = self.sqrt(x)
+            ans = x**.5
         elif self.intPolType == "cubed":
-            ans = self.cubed(x)
+            ans = x**3
         elif self.intPolType == "cubedrt":
-            ans = self.cubert(x)
+            ans = x**float(1/3)
         else:
-            ans = self.linear(x)
+            ans = x
         return ans
-			
-    def linear(self, x):
-        y = x
-        return y
-
-    def smoothStep(self, x):
-        y = (3 * x)**2 - (2 * x)**3
-        return y
-    
-    def squared(self, x):
-        y = x**2
-        return y
-
-    def sqrt(self, x):
-        y = x**.5
-        return y
-
-    def cubed(self, x):
-        y = x**3
-        return y
-
-    def cubert(self, x):
-        y = x**float(1/3)
-        return y
-
-    def sin(self, x):
-        y = math.sin(x)
-        return y
-    
